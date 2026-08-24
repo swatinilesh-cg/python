309bd62b14be018e37bf320cb8e67cd69714235e
swatinilesh-cg <swati.nilesh.cg@gmail.com>
2026-08-24 17:57:09 +0530

    assignment done

---
+++ Variable/Data type/data.py
@@ -0,0 +1,14 @@
+age = 19
+AGE = 20
+AGE = 21
+print("Age:", AGE)
+age = str(age)
+print(age, type(age))
+name = "swati"
+print(name, type(name))
+bool = True
+print(bool, type(bool))
+result = None
+print(result, type(result))
+is_student = str(bool)
+print(is_student, type(is_student))
---
+++ Variable/demo.py
@@ -0,0 +1,107 @@
+
+# Assignment 2 practical ques
+# Q24
+my_name = "swati"
+my_age = 19
+my_city = "delhi"
+print("Name:", my_name)
+print("Age:", my_age)
+print("City:", my_city)
+# Q25
+student_name = "swati"
+student_rollno = 5
+student_branch = "cse"
+print("Name:", student_name)
+print("Roll No:", student_rollno)
+print("Branch:", student_branch)
+# Q26
+marks = 20
+marks = 30
+print("Marks:", marks)
+# Q27
+name ,age,city = "swati", 19, "delhi"
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+# Q28
+x = y = z = 0
+print("x:", x)
+print("y:", y)
+print("z:", z)
+# Q29
+student1 = "swati"
+student_name = "swati"
+student_class = "b.tech"
+
+print("Student 1:", student1)
+print("Student Name:", student_name)
+print("Student Class:", student_class)
+# Q30
+# Student details
+student_name = "swati"
+student_age = 18
+student_age = 19
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+# Assignment 03 practical ques
+# Q24
+student_name = "swati"
+student_age = 19
+student_height = 5.6
+is_student = True
+result = None
+print("Student Name:", student_name, type(student_name))
+print("Student Age:", student_age, type(student_age))
+print("Student Height:", student_height, type(student_height))
+print("Is Student:", is_student, type(is_student))
+print("Result:", result, type(result))
+# Q25
+a = 50
+b = 50.0
+c = "50"
+print("a:", a, type(a))
+print("b:", b, type(b))
+print("c:", c, type(c))
+# Q26
+a = True
+b = False
+print("a:", a, type(a))
+print("b:", b, type(b))
+# Q27
+a = None
+b = "None"
+print("a:", a, type(a))
+print("b:", b, type(b))
+# Q28
+value = 10
+print("Value:", value, type(value))
+value = str(value)
+print("Value:", value, type(value))
+# Q29
+product_name = "Laptop"
+product_quantity = 50
+product_price = 50000.0
+product_available = True
+product_discount = None
+print("Product Name:", product_name, type(product_name))
+print("Product Quantity:", product_quantity, type(product_quantity))
+print("Product Price:", product_price, type(product_price))
+print("Product Available:", product_available, type(product_available))
+print("Product Discount:", product_discount, type(product_discount))
+# Q30
+a = 10
+b = 10.0
+c = "10"
+print("a:", a, type(a))
+print("b:", b, type(b))
+print("c:", c, type(c))
+x = True
+y = "True"
+print("x:", x, type(x))
+print("y:", y, type(y))
+A = None
+B = "None"
+print("A:", A, type(A))
+print("B:", B, type(B))
+
