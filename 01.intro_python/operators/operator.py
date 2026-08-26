775233d5157a8505c4681eeeff5c012dbbad7c1f
swatinilesh-cg <swati.nilesh.cg@gmail.com>
2026-08-26 11:26:58 +0530

    Assignment done

---
+++ 01.intro_python/operators/operator.py
@@ -0,0 +1,88 @@
+print(6**9/7//2%4)
+A = "SWATI"
+B = "SINGH"
+print(A+B)
+# Assignment 04 oues
+#Q1
+a = 4
+b = 6
+print(a+b) 
+print (b-c)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+#Q2
+a = 4
+b = 6.8
+print(a+b , type(a+b)) 
+print (b-c, type(b-c)
+print(a*b , type(a*b))
+print(a/b, type(a/b))
+print(a//b, type(a//b))
+print(a%b, type(a%b))
+print(a**b, type(a**b))
+# Q3
+s1 = 30
+s2 = 20
+s3 = 50
+print (s1 + s2 + s3)
+print ((s1 + s2 + s3)/2)
+# Q4
+product_quantity = 20
+product_price = 100
+total_price = product_quantity * product_price
+print (total_price)
+# Q5
+a = 4
+a%2
+print("even")
+# Q6
+a = 20
+b = 4
+print(a/b)
+print(a//b)
+a = 20
+b = -4
+print(a/b)
+print(a//b)
+# Q7
+a = -4
+b = -6
+print(a+b) 
+print (b-c)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+# Q8
+X = 5
+Y = 6
+W = -7
+Z = -8
+print(X-Y)
+print(X-W)
+print(Z-Y)
+print(W-Z)
+# Q9
+X = 5
+Y = 6
+W = -7
+Z = -8
+print(X//Y)
+print(X//W)
+print(Z//Y)
+print(W//Z)
+# Q10
+X = 5
+Y = 6
+W = -7
+Z = -8
+print(X%Y)
+print(X%W)
+print(Z%Y)
+print(W%Z)
+
+
